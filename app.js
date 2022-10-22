bbb000ad3b2b3f694cfd710f8752bf7e97a03b0b
Oleksandr Vashchenko <76453158+alex-0209@users.noreply.github.com>
2022-10-21 21:41:48 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+function clock() {
+  const fullDate = new Date();
+  let hour = fullDate.getHours();
+  let minute = fullDate.getMinutes();
+  let second = fullDate.getSeconds();
+  
+  
+  const hour_span = document.querySelector('#hour');
+  const minute_span = document.querySelector('#minute');
+  const second_span = document.querySelector('#second');
+  
+  if(hour < 10) { hour = '0' + hour}
+  if(minute < 10) { minute = '0' + minute}
+  if(second < 10) { second = '0' + second}
+
+  hour_span.innerHTML = `${hour} :`;
+  minute_span.innerHTML = `${minute} :`;
+  second_span.innerHTML = `${second} `;
+}
+
+setInterval(clock,1000);
